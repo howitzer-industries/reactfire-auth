761ca13abf59a4b6a858d040b9c585fe15b7988f
Adam Montgomery <montgomery.adam@gmail.com>
2020-04-05 21:42:28 -0500

    add reactfire and firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { FirebaseAppProvider } from "reactfire";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBvXq33jO7G_p7uYh3qjVCR18AxM-zp7As",
+  authDomain: "reactfire-auth-31061.firebaseapp.com",
+  databaseURL: "https://reactfire-auth-31061.firebaseio.com",
+  projectId: "reactfire-auth-31061",
+  storageBucket: "reactfire-auth-31061.appspot.com",
+  messagingSenderId: "768657901723",
+  appId: "1:768657901723:web:16d0c5cc54145c6c659e3e",
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      {/* <PreloadProvider> */}
+      <h1>Working!</h1>
+      {/* </PreloadProvider> */}
+    </FirebaseAppProvider>
   );
 }
 
