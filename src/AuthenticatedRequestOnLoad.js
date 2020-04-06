6164d046ebfdddde3df40b47bb46a80867335e54
Adam Montgomery <montgomery.adam@gmail.com>
2020-04-05 22:00:01 -0500

    make request on load

--- src/AuthenticatedRequestOnLoad.js
+++ src/AuthenticatedRequestOnLoad.js
@@ -1,16 +1,20 @@
 import React from "react";
 
-import {
-  preloadAuth,
-  preloadFirestore,
-  preloadAnalytics,
-  preloadFunctions,
-} from "reactfire";
+import { useFirestore, useUser, useFirestoreCollection } from "reactfire";
 
 export const AuthenticatedRequestOnLoad = (props) => {
+  const user = useUser();
+  const db = useFirestore();
+
+  const collectionRef = db
+    .collectionGroup("invitations")
+    .where("phoneNumber", "==", user.phoneNumber);
+
+  const invitationsCollection = useFirestoreCollection(collectionRef);
   return (
     <>
       <h1>Making an authenticated request</h1>
+      <h2>Returned {invitationsCollection.length} invitations</h2>
     </>
   );
 };
